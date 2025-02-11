12feb832cb45906dc61940994cf8eb9ad261e43a
Jim Sangwine <jim@sangwine.net>
2025-02-11 14:06:27 +0000

    return the entire metadata response and handle errors

--- client/src/utils/updateMetadataWorker.ts
+++ client/src/utils/updateMetadataWorker.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 onmessage = async function (event) {
-  console.log('Received message from the main thread:', event.data);
+  console.log("Received message from the main thread:", event.data);
   const { forceAll } = event.data;
 
   // Perform some computation
-  const updateMetadataResponse = await axios.post(`${process.env.REACT_APP_API_URL}/update-metadata`, { forceAll })
+  const updateMetadataResponse = await axios.post(
+    `${process.env.REACT_APP_API_URL}/update-metadata`,
+    { forceAll }
+  );
 
   // Send the result back to the main thread
-  postMessage(updateMetadataResponse.data);
+  postMessage(updateMetadataResponse);
 };
