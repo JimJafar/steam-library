b7ccfe24ee484afba03dbb5339af180eaf074791
Jim Sangwine <jim@sangwine.net>
2025-02-12 22:57:53 +0000

    fix update script

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
-docker-compose down
+docker compose down
 git add --all
 git commit -m "update metadata"
 git pull
 git push
 docker build -t steam-library:0.1.2 .
-docker-compose up -d
+docker compose up -d
