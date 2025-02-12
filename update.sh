5cc8ff8d89e0517700b166032bb6823bf77b3f33
Jim Sangwine <jim@sangwine.net>
2025-02-12 22:54:55 +0000

    fix update script

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 docker-compose down
-gaa
-gcmsg "update metadata"
-gl
-gp
+git add --all
+git commit -m "update metadata"
+git pull
+git push
 docker build -t steam-library:0.1.2 .
 docker-compose up -d
