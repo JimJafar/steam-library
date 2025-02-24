28c397cccdbe930e2aff0811341a2bc2d2d47fb0
Jim Sangwine <jim@sangwine.net>
2025-02-24 23:53:48 +0000

    return IGDB fields

--- server/utils/mergeMetadata.ts
+++ server/utils/mergeMetadata.ts
@@ -2,19 +2,23 @@ import Game from "types/Game";
 import Metadata from "types/Metadata";
 
 const mergeMetadata = (games: Game[], scores: Metadata[]) => {
-    let matchedMetadata: Metadata | undefined
-    return games.map((game): Metadata | Game => {
-        matchedMetadata = scores.find((score: Metadata) => score.id === game.id)
-        return {
-            ...game,
-            metacriticUrl: matchedMetadata?.metacriticUrl,
-            metacriticScore: matchedMetadata?.metacriticScore,
-            steamScore: matchedMetadata?.steamScore,
-            steamReviewCount: matchedMetadata?.steamReviewCount,
-            onMac: matchedMetadata?.onMac,
-            onDeck: matchedMetadata?.onDeck,
-        }
-    })
-}
+  let matchedMetadata: Metadata | undefined;
+  return games.map((game): Metadata | Game => {
+    matchedMetadata = scores.find((score: Metadata) => score.id === game.id);
+    return {
+      ...game,
+      metacriticUrl: matchedMetadata?.metacriticUrl,
+      metacriticScore: matchedMetadata?.metacriticScore,
+      steamScore: matchedMetadata?.steamScore,
+      steamReviewCount: matchedMetadata?.steamReviewCount,
+      onMac: matchedMetadata?.onMac,
+      onDeck: matchedMetadata?.onDeck,
+      igdbScore: matchedMetadata?.igdbScore,
+      igdbGenres: matchedMetadata?.igdbGenres,
+      igdbTimeToBeat: matchedMetadata?.igdbTimeToBeat,
+      igdbUrl: matchedMetadata?.igdbUrl,
+    };
+  });
+};
 
-export default mergeMetadata
+export default mergeMetadata;
